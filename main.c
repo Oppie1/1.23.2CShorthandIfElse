5679f809fdd9a3c56d451a8f28172b00c8285a12
Oppie1 <atindall729@gmail.com>
2025-05-11 17:40:48 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Write a program using the "?" "if" ":" "else" method. Based off 7 friends.
+// If you have you only have one friend the program will run but not put in "s"
+// Any other number will make program run with "s".
+
 int main()
 {
 	int friends = 7;
